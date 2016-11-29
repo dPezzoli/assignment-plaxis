19d7f9bc6faf44c7ecf93332f8a7681aa9d25d58
Dario <pezzolidario@gmail.com>
2016-11-29 16:06:53 +0100

    new class Car with init

--- src/car.py
+++ src/car.py
@@ -1,3 +1,17 @@
 """
 This module provides an implementation of a car class.
 """
+
+import math
+import shapes_2d
+
+
+class Car:
+    """
+    Class defining a car. Each car is composed by a bunch of 2D shapes. This
+    class provides a method to detect if 'self' collides with another car.
+    """
+
+    def __init__(self, name, shapes):
+        self.name = name
+        self._shapes = shapes

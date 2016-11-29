c7d8417fcd88df62430332f5852c962f53910f27
Dario <pezzolidario@gmail.com>
2016-11-29 17:20:44 +0100

    update UML and changed module description

--- src/shapes_2d.py
+++ src/shapes_2d.py
@@ -4,8 +4,9 @@ differentiate in polygons and non-polygons. Non-polygons are conic sections
 and are defined with a center point. A polygon, on the other hand, may consist
 of a closed polyline which has no center point (the centroid would not provide
 a handful parametrization term for such polygons). However, regular polygons
-may be advantageously defined using their center point. Shapes have the ability
-to detect intersection with other shapes.
+may be advantageously defined using their center point. 
+Besides, this module provide a class able to detect whether two of the modelled
+shapes do overlap each other.
 """
 
 import math

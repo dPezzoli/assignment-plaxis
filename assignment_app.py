e0c13c60f90745dc7928d599e22ad2a3aa1748bb
Dario <pezzolidario@gmail.com>
2016-11-30 15:25:28 +0100

    implemented the new get_intersections routine

--- assignment_app.py
+++ assignment_app.py
@@ -5,3 +5,17 @@ Note: the cars are currently approximated by rectangles or circles.
 
 from src import car
 from src import shapes_2d
+
+
+def get_intersections(cars):
+    """
+    :param cars: The list of cars to be tested
+    :return: A list of intersecting cars pairs
+    """
+    result = []
+
+    for i in range(len(cars)):
+        for j in range(len(cars)):
+            if i < j and cars[i].collides(cars[j]):
+                result.append((cars[i], cars[j]))
+    return result

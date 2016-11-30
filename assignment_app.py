6667db4c873895a35dcba7620d30e650d89ea062
Dario <pezzolidario@gmail.com>
2016-11-30 15:21:21 +0100

    description and imports

---
+++ assignment_app.py
@@ -0,0 +1,7 @@
+"""
+Computes for a list of cars, which of them intersect each other.
+Note: the cars are currently approximated by rectangles or circles.
+"""
+
+from src import car
+from src import shapes_2d

8169a71988bcf70a1b62b0e20c8d27f630469340
Dario <pezzolidario@gmail.com>
2016-12-20 13:29:52 +0100

    Improving shape_2d_test making them more focused

--- tests/shape_2d_test.py
+++ tests/shape_2d_test.py
@@ -91,79 +91,114 @@ class TestOverlappingShapesDetector(unittest.TestCase):
         """
         # Get some shapes to check for collision
         self.circle_a = shapes_2d.Circle((0, 0), 5.0)
-        self.circle_b = shapes_2d.Circle((6, 0), 1.0)
-        self.circle_c = shapes_2d.Circle((6, 0), 1.01)
-        self.circle_d = shapes_2d.Circle((0, 4), 3.0)
-        self.circle_e = shapes_2d.Circle((-9, 4), 0.1)
-        self.rectangle_a = shapes_2d.Rectangle((0, 0), 3.5, 3.9)
-        self.rectangle_b = shapes_2d.Rectangle((4, 0), 0.1, 7.0)
-        self.rectangle_c = shapes_2d.Rectangle((3, 9), 8.4, 0.2)
-        self.rectangle_d = shapes_2d.Rectangle((4, 1), 6.0, 0.1)
-        self.rectangle_e = shapes_2d.Rectangle((16, 2), 2, 1)
+        self.circle_b = shapes_2d.Circle((0, 0), 5.0)
+        self.circle_c = shapes_2d.Circle((6, 0), 2.0)
+        self.circle_d = shapes_2d.Circle((0, 8), 4.0)
+        self.circle_e = shapes_2d.Circle((0, -5), 5.0)
+        self.circle_f = shapes_2d.Circle((-9, 0), 4.0)
+        self.circle_g = shapes_2d.Circle((10 * math.sin(math.pi/4),
+                                          10 * math.sin(math.pi/4)), 4.99)
+        self.circle_h = shapes_2d.Circle((10 * math.sin(math.pi/4),
+                                          10 * math.sin(math.pi/4)), 5.01)
+        self.circle_i = shapes_2d.Circle((15 * math.sin(math.pi/4),
+                                          15 * math.sin(math.pi/4)), 9.99)
+        self.circle_l = shapes_2d.Circle((15 * math.sin(math.pi/4),
+                                          15 * math.sin(math.pi/4)), 10.01)
+        # self.rectangle_a = shapes_2d.Rectangle((0, 0), 3.5, 3.9)
+        # self.rectangle_b = shapes_2d.Rectangle((4, 0), 0.1, 7.0)
+        # self.rectangle_c = shapes_2d.Rectangle((3, 9), 8.4, 0.2)
+        # self.rectangle_d = shapes_2d.Rectangle((4, 1), 6.0, 0.1)
+        # self.rectangle_e = shapes_2d.Rectangle((16, 2), 2, 1)
 
-    def test_do_these_two_shapes_overlap(self):
+    # Tests using circles -----------------------------------------------------
+
+    def test_intersection_perfectly_overlapping_circles(self):
         """
-        Test the possible collision cases
+        Test do_these_two_shapes_overlap method with two perfectly overlapping
+        circles
         """
-        # Test intersection circles
-        self.assertFalse(shapes_2d.OverlappingShapesDetector.
-                         do_these_two_shapes_overlap(self.circle_a,
-                                                     self.circle_b))
-        self.assertFalse(shapes_2d.OverlappingShapesDetector.
-                         do_these_two_shapes_overlap(self.circle_a,
-                                                     self.circle_e))
-        self.assertFalse(shapes_2d.OverlappingShapesDetector.
-                         do_these_two_shapes_overlap(self.circle_b,
-                                                     self.circle_e))
+        self.assertTrue(shapes_2d.OverlappingShapesDetector.
+                        do_these_two_shapes_overlap(self.circle_a,
+                                                    self.circle_b))
+
+    def test_intersection_overlapping_circles_one_unit_positive_x(self):
+        """
+        Test do_these_two_shapes_overlap method with two circles which overlap
+        by one unit at the positive x-axis
+        """
         self.assertTrue(shapes_2d.OverlappingShapesDetector.
                         do_these_two_shapes_overlap(self.circle_a,
                                                     self.circle_c))
+
+    def test_intersection_overlapping_circles_one_unit_positive_y(self):
+        """
+        Test do_these_two_shapes_overlap method with two circles which overlap
+        by one unit at the positive y-axis
+        """
         self.assertTrue(shapes_2d.OverlappingShapesDetector.
-                        do_these_two_shapes_overlap(self.circle_d,
-                                                    self.circle_a))
+                        do_these_two_shapes_overlap(self.circle_a,
+                                                    self.circle_c))
+
+    def test_intersection_offset_circle_by_radius_negative_y(self):
+        """
+        Test do_these_two_shapes_overlap method with two circles which overlap
+        by the radius extension. The two triangles have same radius. One circle
+        center is the axis origin and the other is obtained shifting the first
+        along negative y-axis by a value equal to the radius (the center of one
+        circle is located along the circumference of the other).
+        """
         self.assertTrue(shapes_2d.OverlappingShapesDetector.
                         do_these_two_shapes_overlap(self.circle_b,
-                                                    self.circle_c))
+                                                    self.circle_e))
 
-        # Test intersection rectangles
-        self.assertFalse(shapes_2d.OverlappingShapesDetector.
-                         do_these_two_shapes_overlap(self.rectangle_a,
-                                                     self.rectangle_b))
-        self.assertFalse(shapes_2d.OverlappingShapesDetector.
-                         do_these_two_shapes_overlap(self.rectangle_c,
-                                                     self.rectangle_b))
-        self.assertFalse(shapes_2d.OverlappingShapesDetector.
-                         do_these_two_shapes_overlap(self.rectangle_e,
-                                                     self.rectangle_c))
-        self.assertTrue(shapes_2d.OverlappingShapesDetector.
-                        do_these_two_shapes_overlap(self.rectangle_a,
-                                                    self.rectangle_d))
-        self.assertTrue(shapes_2d.OverlappingShapesDetector.
-                        do_these_two_shapes_overlap(self.rectangle_b,
-                                                    self.rectangle_d))
-
-        # Test intersection circle-rectangle
-        self.assertFalse(shapes_2d.OverlappingShapesDetector.
-                         do_these_two_shapes_overlap(self.circle_e,
-                                                     self.rectangle_c))
+    def test_intersection_not_overlapping_circles_one_unit_negative_x(self):
+        """
+        Test do_these_two_shapes_overlap method with two circles which touch at
+        negative y-axis. If only one point along the circumference is shared by
+        the triangles, there is no intersection.
+        """
         self.assertFalse(shapes_2d.OverlappingShapesDetector.
                          do_these_two_shapes_overlap(self.circle_a,
-                                                     self.rectangle_e))
+                                                     self.circle_f))
+
+    def test_intersection_not_overlapping_circles_first_quadrant(self):
+        """
+        Test do_these_two_shapes_overlap method with two circles which touch
+        along the bisecting line of the first quadrant. If only one point along
+        the circumference is shared by the triangles, there is no intersection.
+        """
         self.assertFalse(shapes_2d.OverlappingShapesDetector.
-                         do_these_two_shapes_overlap(self.circle_c,
-                                                     self.rectangle_a))
-        self.assertFalse(shapes_2d.OverlappingShapesDetector.
-                         do_these_two_shapes_overlap(self.circle_c,
-                                                     self.rectangle_b))
+                         do_these_two_shapes_overlap(self.circle_a,
+                                                     self.circle_g))
+
+    def test_intersection_overlapping_circles_first_quadrant(self):
+        """
+        Test do_these_two_shapes_overlap method with two circles which overlap
+        along the bisecting line of the first quadrant.
+        """
         self.assertTrue(shapes_2d.OverlappingShapesDetector.
                         do_these_two_shapes_overlap(self.circle_a,
-                                                    self.rectangle_b))
+                                                    self.circle_h))
+
+    def test_intersection_not_overlapping_circles_third_quadrant(self):
+        """
+        Test do_these_two_shapes_overlap method with two circles which touch
+        along the bisecting line of the third quadrant. If only one point along
+        the circumference is shared by the triangles, there is no intersection.
+        """
+        self.assertFalse(shapes_2d.OverlappingShapesDetector.
+                         do_these_two_shapes_overlap(self.circle_a,
+                                                     self.circle_g))
+
+    def test_intersection_overlapping_circles_third_quadrant(self):
+        """
+        Test do_these_two_shapes_overlap method with two circles which overlap
+        along the bisecting line of the third quadrant.
+        """
         self.assertTrue(shapes_2d.OverlappingShapesDetector.
                         do_these_two_shapes_overlap(self.circle_a,
-                                                    self.rectangle_a))
-        self.assertTrue(shapes_2d.OverlappingShapesDetector.
-                        do_these_two_shapes_overlap(self.circle_d,
-                                                    self.rectangle_d))
+                                                    self.circle_h))
+
 
 if __name__ == "__main__":
     unittest.main()

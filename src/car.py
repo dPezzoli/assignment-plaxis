bbed3d9cdac103340725ee01c65ffe847c207f56
Dario <pezzolidario@gmail.com>
2016-12-21 09:12:14 +0100

    Remove computeBBox from Car

--- src/car.py
+++ src/car.py
@@ -2,8 +2,7 @@
 This module provides an implementation of a car class.
 """
 
-import math
-from src import shapes_2d
+from src import shapes_2d, overlaps_detection
 
 
 class Car:
@@ -23,32 +22,6 @@ class Car:
     @property
     def name(self):
         return self._name
-
-    def compute_bounding_box(self):
-        """
-        :return: The min and max coordinates points of this car axis aligned
-                 bounding box
-        """
-        min_coordinates = [math.inf, math.inf]
-        max_coordinates = [-math.inf, -math.inf]
-
-        for shape in self._shapes:
-
-            shape_bounding_box = shape.get_bounding_box()
-
-            shape_min_coordinates = shape_bounding_box[0]
-            if shape_min_coordinates[0] < min_coordinates[0]:
-                min_coordinates[0] = shape_min_coordinates[0]
-            if shape_min_coordinates[1] < min_coordinates[1]:
-                min_coordinates[1] = shape_min_coordinates[1]
-
-            shape_max_coordinates = shape_bounding_box[1]
-            if shape_max_coordinates[0] > max_coordinates[0]:
-                max_coordinates[0] = shape_max_coordinates[0]
-            if shape_max_coordinates[1] > max_coordinates[1]:
-                max_coordinates[1] = shape_max_coordinates[1]
-
-        return min_coordinates, max_coordinates
 
     def collides(self, car):
         """

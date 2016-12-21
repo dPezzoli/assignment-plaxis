58eef6d89b7040934907c850b5c7d78e27ac88b7
Dario <pezzolidario@gmail.com>
2016-12-21 09:16:30 +0100

    removing test_compute_bounding_box from car_test

--- tests/car_test.py
+++ tests/car_test.py
@@ -42,26 +42,6 @@ class TestCar(unittest.TestCase):
         self.shapes_fourth_car.append(shapes_2d.Circle((10, 3), 1))
         self.fourth_car = car.Car("Maserati", self.shapes_fourth_car)
 
-    def test_compute_bounding_box(self):
-        """
-        Tests min max coordinates of axis aligned bounding boxes
-        """
-        box_first_car = self.first_car.compute_bounding_box()
-        self.assertEqual(box_first_car[0], [0, -1])
-        self.assertEqual(box_first_car[1], [7, 3])
-
-        box_second_car = self.second_car.compute_bounding_box()
-        self.assertEqual(box_second_car[0], [8, -1])
-        self.assertEqual(box_second_car[1], [16, 5])
-
-        box_third_car = self.third_car.compute_bounding_box()
-        self.assertEqual(box_third_car[0], [2, 4])
-        self.assertEqual(box_third_car[1], [8, 7])
-
-        box_fourth_car = self.fourth_car.compute_bounding_box()
-        self.assertEqual(box_fourth_car[0], [5, 2])
-        self.assertEqual(box_fourth_car[1], [11, 5])
-
     def test_collides(self):
         """
         Tests cars collisions
